ad51f0446eaa362271f9050dff331834d022cff4
rustemmanafov <rustemmanafov1@gmail.com>
2022-11-04 02:00:34 +0400

    updated

--- MyMusic/PlayerViewController.swift
+++ MyMusic/PlayerViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayerViewController: UIViewController {
     
@@ -13,23 +14,115 @@ class PlayerViewController: UIViewController {
     public var songs: [Song] = []
     
     @IBOutlet var holder: UIView!
-
+    
+    var player: AVAudioPlayer?
+    
+    private let albumImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .top
+        return imageView
+    }()
+    
+    private let songNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    private let artistNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    private let albumNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        if holder.subviews.count == 1{
+        if holder.subviews.count == 0 {
             configure()
         }
     }
-
+    
     func configure() {
+        let song = songs[position]
+        
+        let urlString = Bundle.main.path(forResource: song.trackName, ofType: "mp3")
+        
+        do {
+            try AVAudioSession.sharedInstance().setMode(.default)
+            try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+            
+            guard let urlString = urlString else {
+                return
+            }
+            
+            player = try AVAudioPlayer(contentsOf: URL(string: urlString)!)
+            
+            guard let player = player else {
+                return
+            }
+            player.play()
+        }
+        
+        catch {
+            print("error")
+        }
+        
+        albumImageView.frame = CGRect(x: 10,
+                                      y: 10,
+                                      width: holder.frame.size.width - 20,
+                                      height: holder.frame.size.height - 20)
+        albumImageView.image = UIImage(named: song.imageName)
+        holder.addSubview(albumImageView)
+        
+        songNameLabel.frame = CGRect(x: 10,
+                                     y: albumImageView.frame.size.height + 10,
+                                     width: holder.frame.size.width - 20,
+                                     height: 70)
+        
+        artistNameLabel.frame = CGRect(x: 10,
+                                       y: albumImageView.frame.size.height + 10 + 140,
+                                       width: holder.frame.size.width - 20,
+                                       height: 70)
+        
+        albumNameLabel.frame = CGRect(x: 10,
+                                      y: albumImageView.frame.size.height + 10 + 70,
+                                      width: holder.frame.size.width - 20,
+                                      height: 70)
+        
+        
+        songNameLabel.text = song.name
+        artistNameLabel.text = song.artistName
+        albumNameLabel.text = song.albumName
+        
+        holder.addSubview(songNameLabel)
+        holder.addSubview(artistNameLabel)
+        holder.addSubview(albumNameLabel)
+        
         
     }
-  
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if let player = player {
+            player.stop()
+        }
+    }
+    
 }
